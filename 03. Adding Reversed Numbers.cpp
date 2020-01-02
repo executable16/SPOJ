fcd80916659a5eb1721a0940ef2ac93fd9c3fda9
Atul Malakar <atulmalakar15@gmail.com>
2020-01-02 20:16:50 +0530

    Create 03. Adding Reversed Numbers.cpp

---
+++ 03. Adding Reversed Numbers.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long int ull;
+typedef long long int ll;
+const ull MOD=1e9+7;
+const int INF = 0x3f3f3f3f;// for integer only
+#define PI 3.1415926535897932384626
+#define flash ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#define debug(x) cout << (#x) << " is " << (x) << endl
+#define rep(i,j,k) for(int i=j;i<k;i++)
+#define TC int tc;cin>>tc;while(tc--)
+#define umap unordered_map
+#define uset unordered_set
+#define vi vector<int>
+#define pb emplace_back
+#define po pop_back
+#define vsort(x) sort(x.begin(),x.end())
+#define vrsort(x) sort(x.begin(),x.end(),greater<int>())
+#define pii pair<int,int>
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+ll reverse(ll x){
+	ll rev = 0;
+	while(x > 0){
+		rev = rev*10 + x%10;
+		x/=10;
+	}
+	return rev;
+}
+int main(){
+	TC{
+		ll x,y;
+		cin>>x>>y;
+		cout<<reverse(reverse(x) + reverse(y))<<endl; 
+	}
+}
