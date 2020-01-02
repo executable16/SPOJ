9418ea05648f1c5e7852c629e3519d457fc1faae
Atul Malakar <atulmalakar15@gmail.com>
2020-01-02 20:15:15 +0530

    Create 01. TEST.cpp

---
+++ 01. TEST.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+typedef unsigned long long int ll;
+#define MOD 1000000007
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+while(true)
+{   int n;
+    cin>>n;
+    if(n!=42)
+    cout<<n<<endl;
+    else
+    break;
+}
+}
